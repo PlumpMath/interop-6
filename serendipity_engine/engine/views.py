9cc282cff0d9128ef0206ef12a1f609cab904649
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-10-31 15:47:31 -0400

    special registration workflow

--- serendipity_engine/engine/views.py
+++ serendipity_engine/engine/views.py
@@ -1,4 +1,15 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.db.models.signals import pre_save
 from django.views.generic.base import TemplateView
+
+from registration.forms import RegistrationFormUniqueEmail
+from registration.models import RegistrationProfile
+from registration.views import RegistrationView
+
 from serendipity_engine.projects.models import Project
 
 class HomeView(TemplateView):
@@ -19,3 +30,59 @@ class MiscellaneousView(HomeView):
         context = super(MiscellaneousView, self).get_context_data(**kwargs)
         context['miscellaenous'] = True
         return context
+
+class SerendipityRegistrationView(RegistrationView):
+    form_class = RegistrationFormUniqueEmail
+    success_url = reverse_lazy('registration_complete')
+    
+    def register(self, request, **cleaned_data):
+        username, email, password = cleaned_data['username'], cleaned_data['email'], cleaned_data['password1']
+        if Site._meta.installed:
+            site = Site.objects.get_current()
+        else:
+            site = RequestSite(request)
+        new_user = RegistrationProfile.objects.create_inactive_user(username, email,
+                                                                    password, site)
+        
+        domain = Site.objects.get_current().domain
+        send_mail('New interop user needs activation',
+                  'An account for ' + username + ' with email address '
+                  + email + ' has just been registered at the Harvard Interop '
+                  'Serendipity Engine. If this is legit, please log into the '
+                  'admin site (http://' + domain + reverse('admin:index') +
+                  ') and activate the account.',
+                  settings.FROM_EMAIL,
+                  settings.INTEROP_ADMINS)
+        return new_user
+
+
+def activate_user(sender, **kwargs):
+    try:
+        instance = kwargs['instance']
+        user = User.objects.get(username=instance.username)
+        domain = Site.objects.get_current().domain
+        if (user.is_active == False and instance.is_active == True):
+            send_mail('Woot! Your Harvard Interop account is live.',
+                      'Your Harvard interop account has been approved. '
+                      'You can now log in at http://' + domain +
+                      reverse('login') + ' with the username '
+                      + instance.username + ' and the password you set when '
+                      'you created your account.\n\n' 
+                      'Forgot your password? You can reset it at http://' +
+                      domain + reverse('django.contrib.auth.views.password_reset')
+                      + ' .',
+                      settings.FROM_EMAIL,
+                      settings.INTEROP_ADMINS)
+    
+    except User.DoesNotExist:
+        # if User.DoesNotExist, we're registering and haven't saved them to db yet
+        # no need to do anything; we only send activation emails on the second
+        # step of our two-step registration
+        pass
+pre_save.connect(activate_user, sender=User)
+"""TODO:        
+  define an activation view that emails the user
+  make sure the emails the user gets on registration and activation
+  make sense. in particular, no activation key! And remind about username.
+  test and make sure it works.
+"""
