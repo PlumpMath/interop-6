4fdf48905b3886d9aa4cd1cfb46aad326c84dc3c
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-11-05 14:48:42 -0500

    styling

---
+++ serendipity_engine/units/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Unit, School
+
+class UnitUpdateForm(forms.ModelForm):
+    # Makes it possible to associate units with schools through front end.
+    school = forms.ModelChoiceField(queryset=School.objects.all().order_by('name'),
+                                    empty_label="(No school)")
+
+    class Meta:
+        model = Unit
+        fields = ('name', 'url')
+        
+class SchoolUpdateForm(forms.ModelForm):
+    # Overrides the default widget to allow for empty input.
+    subunits = forms.ModelMultipleChoiceField(queryset=Unit.objects.all(),
+                                              required=False)
+    
+    class Meta:
+        model = School
+        fields = ('subunits',)
